b68d808cc47c936b1facaea6ea1fd0a2dbc097c2
Lucio <lucio.oliveira@zup.com.br>
2021-12-08 21:41:21 -0300

    contatos

--- aula_08_12_2021/index.js
+++ aula_08_12_2021/index.js
@@ -4,13 +4,15 @@ const client = require('./server/contatos')
 http.createServer(async (req, res) => {
     const url = req.url
     const method = req.method
-
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     if (method === 'GET' && url.includes('/contatos')) {
         const contatos = await client.getAll()
         console.log(JSON.stringify(contatos))
         res.end(JSON.stringify(contatos))
     } else if (method === 'POST' && url.includes('/contatos')) {
-        res.end("POST contatos")
+        const body = await getBody(req)
+        const contato = await client.save(body)
+        res.end(JSON.stringify(contato))
     } else if (method === 'PUT' && url.includes('/contatos')) {
         res.end("PUT contatos")
     } else if (method === 'DELETE' && url.includes('/contatos')) {
@@ -21,3 +23,15 @@ http.createServer(async (req, res) => {
     }
 
 }).listen(3000, () => console.log("Servidor UP"))
+
+function getBody(req) {
+    return new Promise((resolve) => {
+      let chunks = '';
+      req.on('data', chunk => {
+        chunks+=chunk.toString();
+      });
+      req.on('end', () => {
+        resolve(JSON.parse(chunks))
+      });
+    });
+}  
--- aula_08_12_2021/server/contatos.js
+++ aula_08_12_2021/server/contatos.js
@@ -9,6 +9,25 @@ const client = new Client({
 })
 client.connect()
 
+function last_id() {
+    return client.query('SELECT MAX(contato_id) FROM contato')
+        .then(result => result.rows[0].max).catch(console.log)
+}
+
+async function save(contato) {
+    const id = await last_id()
+    console.log(id)
+    const query = {
+        text: 'insert into contato values ($1, $2, $3, $4, $5)',
+        values: [id + 1, contato.apelido, contato.email, contato.endereco, contato.telefone]
+    }
+    return client
+        .query(query)
+        .then(res => res.rows[0])
+}
+
+
+
 async function getAll() {
     const result = await client.query('SELECT * FROM contato')
     return result.rows.map(contato => {
@@ -24,6 +43,6 @@ async function getAll() {
 
 module.exports = {
     client: client,
-    getAll: getAll
+    getAll: getAll,
+    save: save
 }
-
