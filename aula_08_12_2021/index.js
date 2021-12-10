c3feac1def085100849242bbee26488b63e09d94
Lucio <lucio.oliveira@zup.com.br>
2021-12-10 20:31:34 -0300

    finalizando api

--- aula_08_12_2021/index.js
+++ aula_08_12_2021/index.js
@@ -5,33 +5,53 @@ http.createServer(async (req, res) => {
     const url = req.url
     const method = req.method
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    if (method === 'GET' && url.includes('/contatos')) {
-        const contatos = await client.getAll()
-        console.log(JSON.stringify(contatos))
-        res.end(JSON.stringify(contatos))
-    } else if (method === 'POST' && url.includes('/contatos')) {
-        const body = await getBody(req)
-        const contato = await client.save(body)
-        res.end(JSON.stringify(contato))
-    } else if (method === 'PUT' && url.includes('/contatos')) {
-        res.end("PUT contatos")
-    } else if (method === 'DELETE' && url.includes('/contatos')) {
-        res.end("DELETE contatos")
-    } else {
-        res.writeHead(404);
-        res.end('not found')
+    try {
+        if (method === 'GET' && url.includes('/contatos/')) {
+            const id = getId(url)
+            const contato = await client.getById(id)
+            res.end(JSON.stringify(contato))
+        } else if (method === 'GET' && url.includes('/contatos')) {
+            const contatos = await client.getAll()
+            console.log(JSON.stringify(contatos))
+            res.end(JSON.stringify(contatos))
+        } else if (method === 'POST' && url.includes('/contatos')) {
+            const body = await getBody(req)
+            const contato = await client.save(body)
+            res.end(JSON.stringify(contato))
+        } else if (method === 'PUT' && url.includes('/contatos/')) {
+            const id = getId(url)
+            const body = await getBody(req)
+            const new_contato = await client.updateById(id, body)
+            res.end(JSON.stringify(new_contato))
+        } else if (method === 'DELETE' && url.includes('/contatos/')) {
+            const id = getId(url)
+            await client.deleteById(id)
+            res.end()
+        } else {
+            res.writeHead(404);
+            res.end('not found')
+        }
+    } catch (error) {
+        if(error.status){
+            res.writeHead(error.status);
+        } else res.writeHead(500);
+        res.end(JSON.stringify(error))
     }
 
 }).listen(3000, () => console.log("Servidor UP"))
 
 function getBody(req) {
     return new Promise((resolve) => {
-      let chunks = '';
-      req.on('data', chunk => {
-        chunks+=chunk.toString();
-      });
-      req.on('end', () => {
-        resolve(JSON.parse(chunks))
-      });
+        let chunks = '';
+        req.on('data', chunk => {
+            chunks += chunk.toString();
+        });
+        req.on('end', () => {
+            resolve(JSON.parse(chunks))
+        });
     });
-}  
+}
+
+function getId(url) {
+    return url.substring('/contatos/'.length)
+}
--- aula_08_12_2021/server/contatos.js
+++ aula_08_12_2021/server/contatos.js
@@ -16,7 +16,6 @@ function last_id() {
 
 async function save(contato) {
     const id = await last_id()
-    console.log(id)
     const query = {
         text: 'insert into contato values ($1, $2, $3, $4, $5)',
         values: [id + 1, contato.apelido, contato.email, contato.endereco, contato.telefone]
@@ -26,23 +25,56 @@ async function save(contato) {
         .then(res => res.rows[0])
 }
 
+async function getById(id){
+    const result = await client.query('SELECT * FROM contato c where c.contato_id = $1', [id])
+    if(!result.rowCount){
+        throw {error: "not found", status: 404}
+    }
+    return result.rows.map(convertContatoToContatoDTO)[0]
+}
+
+async function updateById(id, contato){
+    const query = {
+        text: 'UPDATE contato SET apelido = $1, email = $2, endereco = $3, telefone = $4 WHERE contato_id = $5',
+        values: [contato.apelido, contato.email, contato.endereco, contato.telefone, id]
+    }
+    return client
+        .query(query)
+        .then((res) => getById(id))
+}
+
+async function deleteById(id) {
+    const query = {
+        text: 'DELETE from contato WHERE contato_id = $1',
+        values: [id]
+    }
+    const result = await client.query(query)
+    if(!result.rowCount){
+        throw {error: "not found", status: 404}
+    }
+}
 
 
 async function getAll() {
     const result = await client.query('SELECT * FROM contato')
-    return result.rows.map(contato => {
-        return {
-            id: contato.contato_id,
-            apelido: contato.apelido,
-            email: contato.email,
-            endereco: contato.endereco,
-            telefone: contato.telefone
-        }
-    })
+    return result.rows.map(convertContatoToContatoDTO)
+}
+
+function convertContatoToContatoDTO(contato) {
+    return {
+        id: contato.contato_id,
+        apelido: contato.apelido,
+        email: contato.email,
+        endereco: contato.endereco,
+        telefone: contato.telefone
+    }
 }
 
 module.exports = {
     client: client,
     getAll: getAll,
-    save: save
+    save: save,
+    getById,
+    updateById,
+    deleteById
 }
